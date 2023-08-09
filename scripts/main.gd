face7c73731504ae19bba718767f4fe8e6f7fa1e
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-08-09 19:03:44 -0400

    shader tweaks, mousemode

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,19 @@
 extends Node
 @onready var firstship = $"frame/ship"
-
+var mousemode:bool
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	firstship.addBlock(Vector3(0,0,0), Quaternion(0,0,0,1), "structure_block")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_released("right_click"):
+		mousemode = not mousemode
+		if mousemode:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _input(input):
 	if Input.is_action_just_pressed("toggle_fullscreen"):
