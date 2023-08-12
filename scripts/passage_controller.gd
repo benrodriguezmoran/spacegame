97b3ae7e75d938ec5682eca5b90413def44bcb91
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-08-12 15:07:09 -0400

    passage walls complete

--- scripts/passage_controller.gd
+++ scripts/passage_controller.gd
@@ -2,14 +2,14 @@ extends Node
 @export var colliders = []
 @export var walls = []
 @onready var block 
-enum {FORE,BACK,LEFT,RIGHT,UP,DOWN}
+enum enumDirection {FORE,BACK,LEFT,RIGHT,UP,DOWN}
 var direction = {
-	"FORE":Vector3(1,0,0),
-	"BACK":Vector3(-1,0,0),
-	"LEFT":Vector3(0,0,-1),
-	"RIGHT":Vector3(0,0,1),
-	"UP":Vector3(0,1,0),
-	"DOWN":Vector3(0,-1,0),
+	Vector3(1,0,0):[enumDirection.FORE,enumDirection.BACK],
+	Vector3(-1,0,0):[enumDirection.BACK,enumDirection.FORE],
+	Vector3(0,0,-1):[enumDirection.LEFT,enumDirection.RIGHT],
+	Vector3(0,0,1):[enumDirection.RIGHT,enumDirection.LEFT],
+	Vector3(0,1,0):[enumDirection.UP,enumDirection.DOWN],
+	Vector3(0,-1,0):[enumDirection.DOWN,enumDirection.UP],
 }
 var neighbors
 
@@ -24,9 +24,9 @@ func _process(delta):
 
 func _on_passage_type_set():
 	block = get_parent()
-	block.parentShip.connect("block_added", Callable(self, "block_added"))
+	block.parentShip.connect("block_added", Callable(self, "on_block_added"))
 	
-func block_added(placePos):
+func on_block_added(placePos):
 	var blockPos = block.parentShip.blocks.find_key(block)
 	if placePos != blockPos: return
 	for child in get_parent().get_children():
@@ -35,57 +35,14 @@ func block_added(placePos):
 	colliders = block.colliders
 	block.rotation = Vector3(0,0,0)
 	neighbors = block.parentShip.get_neighbors(blockPos, block.parentShip.passageDictionary)
-	for neighbor in neighbors:
-		var neighborBlock = block.parentShip.blocks.get(neighbor)
-		var blockRelative = blockPos - neighbor
-		print("calling neighbor")
-		match blockRelative:
-			direction.FORE: 
-				print("FORE")
-				var checkDirection = FORE
-				var invCheck = BACK
-				colliders[checkDirection].disabled = true
-				walls[checkDirection].visible = false
-				neighborBlock.get_node("passage_controller").colliders[invCheck].disabled = true
-				neighborBlock.get_node("passage_controller").walls[invCheck].visible = false
-			direction.BACK: 
-				print("BACK")
-				var checkDirection = BACK
-				var invCheck = FORE
-				colliders[checkDirection].disabled = true
-				walls[checkDirection].visible = false
-				neighborBlock.get_node("passage_controller").colliders[invCheck].disabled = true
-				neighborBlock.get_node("passage_controller").walls[invCheck].visible = false
-			direction.LEFT: 
-				print("LEFT")
-				var checkDirection = LEFT
-				var invCheck = RIGHT
-				colliders[checkDirection].disabled = true
-				walls[checkDirection].visible = false
-				neighborBlock.get_node("passage_controller").colliders[invCheck].disabled = true
-				neighborBlock.get_node("passage_controller").walls[invCheck].visible = false
-			direction.RIGHT: 
-				print("RIGHT")
-				var checkDirection = RIGHT
-				var invCheck = LEFT
-				colliders[checkDirection].disabled = true
-				walls[checkDirection].visible = false
-				neighborBlock.get_node("passage_controller").colliders[invCheck].disabled = true
-				neighborBlock.get_node("passage_controller").walls[invCheck].visible = false
-			direction.DOWN: 
-				print("DOWN")
-				var checkDirection = DOWN
-				var invCheck = UP
-				colliders[checkDirection].disabled = true
-				walls[invCheck].visible = false
-				neighborBlock.get_node("passage_controller").colliders[invCheck].disabled = true
-				neighborBlock.get_node("passage_controller").walls[checkDirection].visible = false
-			direction.UP: 
-				print("UP")
-				var checkDirection = UP
-				var invCheck = DOWN
-				colliders[checkDirection].disabled = true
-				walls[invCheck].visible = false
-				neighborBlock.get_node("passage_controller").colliders[invCheck].disabled = true
-				neighborBlock.get_node("passage_controller").walls[checkDirection].visible = false
-	print(neighbors)
+	for neighborPos in neighbors:
+		var neighborBlock = block.parentShip.blocks.get(neighborPos)
+		var blockRelative = blockPos - neighborPos
+		var neighborPassage = neighborBlock.get_node("passage_controller")
+		if direction.has(blockRelative):
+			var checkDirection = direction[blockRelative][0]
+			var invCheckDirection = direction[blockRelative][1]
+			colliders[checkDirection].disabled = true
+			walls[checkDirection].visible = false
+			neighborPassage.colliders[invCheckDirection].disabled = true
+			neighborPassage.walls[invCheckDirection].visible = false
